720deb2e287beccb2a30ae41fc3cdc7c4027ec1a
Matt Thompson <mattt@defunct.ca>
2014-07-22 13:25:45 +0100

    Initial pass at glance check
    This simply checks glance running on localhost by doing a findall on
    all active images.  If no active images are found, we throw an error.
    
    I've resorted to using OpenStack clients but we could revert to querying
    APIs directly for more 'light-weight' checks.
    
    We will also need some sort of API token caching otherwise we're going
    to generate a lot of keystone traffic from all the monitor checks.

---
+++ glance_check.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from glanceclient import Client, exc
+from keystoneclient.v2_0 import client
+from keystoneclient.openstack.common.apiclient import exceptions
+import os
+import sys
+
+OS_IMAGE_ENDPOINT = 'http://127.0.0.1:9292'
+
+
+def set_auth_details():
+    auth_details = {'OS_USERNAME': None,
+                    'OS_PASSWORD': None,
+                    'OS_TENANT_NAME': None,
+                    'OS_AUTH_URL': None}
+
+    for key in auth_details.keys():
+        if key in os.environ:
+            auth_details[key] = os.environ[key]
+
+    return auth_details
+
+
+def main():
+    auth_details = set_auth_details()
+
+    for key, value in auth_details.items():
+        if value is None:
+            print "status err os.environ['%s'] not set" % key
+            sys.exit(1)
+
+    try:
+        keystone = client.Client(username=auth_details['OS_USERNAME'],
+                                 password=auth_details['OS_PASSWORD'],
+                                 tenant_name=auth_details['OS_TENANT_NAME'],
+                                 auth_url=auth_details['OS_AUTH_URL'])
+    except (exceptions.Unauthorized, exceptions.AuthorizationFailure) as e:
+        print "status err %s" % e
+        sys.exit(1)
+
+    os_auth_token = keystone.auth_ref['token']['id']
+
+    glance = Client('1', endpoint=OS_IMAGE_ENDPOINT, token=os_auth_token)
+
+    try:
+        images = glance.images.findall(status="active")
+    except (exc.CommunicationError, exc.HTTPInternalServerError) as e:
+        print "status err %s" % e
+        sys.exit(1)
+
+    if len(images) < 1:
+        print 'status err no glance images found'
+    else:
+        print 'status OK'
+
+if __name__ == "__main__":
+    main()
