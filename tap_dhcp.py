198cee51ead0074367a1553d6a61ddfc6e12f202
Ian Cordasco <ian.cordasco@rackspace.com>
2014-08-11 11:49:21 -0500

    Clean up tap_dhcp check and add monitor for disconnected veth pairs

--- tap_dhcp.py
+++ tap_dhcp.py
@@ -1,13 +1,18 @@
 import re
 import subprocess
 
-from maas_common import metric, status_ok, status_err
+from maas_common import metric, metric_bool, status_ok, status_err
 
 
 def get_namespace_list():
     """Retrieve the list of DHCP namespaces."""
     namespaces = subprocess.check_output(['ip', 'netns', 'list']).split()
-    return list(filter(lambda n: n.startswith('qdhcp-'), namespaces))
+    ns_list = list(filter(lambda n: n.startswith('qdhcp-'), namespaces))
+
+    if not ns_list:
+        status_err('no dhcp namespaces on this host')
+
+    return ns_list
 
 
 def get_interfaces_for(namespace):
@@ -17,12 +22,43 @@ def get_interfaces_for(namespace):
     ]).split('\n')
 
 
+def get_bridges():
+    """Retrieve a list of bridges from brctl."""
+    brctl = subprocess.check_output(['brctl', 'show']).split('\n')
+    return [
+        i.split('\t')[0] for i in brctl
+        if i and not i.startswith((' ', '\t', 'bridge name'))
+    ]
+
+
+def generate_network_device_details(bridges):
+    """Generate a list of network device details from ip."""
+    ip = subprocess.check_output(['ip', 'a']).split('\n')
+    ignored_devices = set(['lo'] + bridges)
+    for line in ip:
+        if line.startswith(' '):
+            continue
+
+        (_, name, details) = line.split(': ')
+        if name in ignored_devices:
+            continue
+
+        yield details
+
+
+def check_for_disconnected_veth_pairs():
+    """Return True if there are disconnected pairs else False."""
+    bridges = get_bridges()
+    return any(
+        all(bridge_name not in detail for bridge_name in bridges)
+        for detail in generate_network_device_details(bridges)
+    )
+
+
 def main():
     TAP = re.compile('^\d+: .*state [A-Z]+$')
     LOOP = re.compile('^\d+: lo')
     namespaces = get_namespace_list()
-    if not namespaces:
-        status_err('no dhcp namespaces on this host')
 
     interfaces = ((n, get_interfaces_for(n)) for n in namespaces)
     too_many_taps = []
@@ -42,6 +78,9 @@ def main():
     if number_of_namespaces > 0:
         for (name, number) in too_many_taps:
             metric(name, 'uint32', number)
+
+    metric_bool('disconnected_veth_pair_exists',
+                check_for_disconnected_veth_pairs())
 
 
 if __name__ == '__main__':
