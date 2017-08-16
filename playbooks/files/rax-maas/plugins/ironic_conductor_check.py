e2a13edfb24d2d21d8c9a0d73485275815d016d2
tonytan4ever <tony.tan@rackspace.com>
2017-08-16 12:24:54 -0400

    TURTLES-111, partially address RT-30, Release note: Add ironic conductor check to ironic-maas to ensure ironic conductors are running.

---
+++ playbooks/files/rax-maas/plugins/ironic_conductor_check.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+# Copyright 2014, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+
+import psutil
+
+from maas_common import get_auth_ref
+from maas_common import metric_bool
+from maas_common import print_output
+
+
+def check(auth_ref, args):
+    name = "ironic-conductor_status"
+    for proc in psutil.process_iter():
+        if 'ironic-conducto' in proc.name():
+            metric_bool(name, True)
+            break
+    else:
+        metric_bool(name, False)
+
+
+def main(args):
+    auth_ref = get_auth_ref()
+    check(auth_ref, args)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Check Ironic conductor status')
+    parser.add_argument('--telegraf-output',
+                        action='store_true',
+                        default=False,
+                        help='Set the output format to telegraf')
+    args = parser.parse_args()
+    with print_output(print_telegraf=args.telegraf_output):
+        main(args)
