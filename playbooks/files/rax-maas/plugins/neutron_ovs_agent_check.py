66b0b49a89f536eb29b75e07236251fa9336c879
tonytan4ever <tony.tan@rackspace.com>
2017-11-14 23:24:41 -0600

    TURTLES-354: Improvement on neutron OVS agent check script.
    1. Take into consideration of running on compute node
    2. Add alarms for ovsdb-server and ovs-vswitchd
    3. Update README to let metric include hostname.

--- playbooks/files/rax-maas/plugins/neutron_ovs_agent_check.py
+++ playbooks/files/rax-maas/plugins/neutron_ovs_agent_check.py
@@ -17,7 +17,12 @@ import argparse
 import os
 import re
 
-import lxc
+try:
+    import lxc
+    on_lxc_container = True
+except ImportError:
+    import socket
+    on_lxc_container = False
 
 from maas_common import metric_bool
 from maas_common import print_output
@@ -25,87 +30,119 @@ from maas_common import status_err
 from process_check_container import get_processes
 
 
-def check(args):
+def check_process_statuses(container_or_host_name, container=None):
+    process_names = ['ovsdb-server', 'ovs-vswitchd',
+                     'neutron-openvswitch-agent']
 
-    containers = lxc.list_containers()
-    neutron_agent_container = None
-    for container in containers:
-        if 'neutron_agents' in container:
-            neutron_agent_container = container
-            metric_bool('agents_found', True, m_name='maas_neutron')
-            break
+    if container is None:
+        pid = None
     else:
-        metric_bool('agents_found', False, m_name='maas_neutron')
-        status_err('no running neutron agents containers found',
-                   m_name='maas_neutron')
-        return
+        pid = container.init_pid
 
-    # Get the neutron_agent_container's init PID.
-    try:
-        c = lxc.Container(neutron_agent_container)
-        # If the container wasn't found, exit now.
-        if c.init_pid == -1:
-            metric_bool('container_success', False,
-                        m_name='maas_neutron_agent_container')
-            status_err(
-                'Could not find PID for container {}'.format(
-                    neutron_agent_container
-                ),
-                m_name='maas_neutron_agent_container'
-            )
-    except (Exception, SystemError) as e:
-        metric_bool('container_success', False,
-                    m_name='maas_neutron_agent_container')
-        status_err(
-            'Container lookup failed on "{}". ERROR: "{}"'.format(
-                neutron_agent_container,
-                e
-            ),
-            m_name='maas_neutron_agent_container'
-        )
-    else:
-        metric_bool('container_success', True,
-                    m_name='maas_neutron_agent_container')
+    # Get the processes within the neutron agent container (or a
+    # compute host).
+    procs = get_processes(parent_pid=pid)
 
-    # Get the processes within the neutron agent container.
-    procs = get_processes(parent_pid=c.init_pid)
-
-    # Make a list of command lines from each PID. There's a chance that one or
-    # more PIDs may have exited already and this causes a NoSuchProcess
-    # exception.
+    # Make a list of command lines from each PID. There's a
+    # chance that one or more PIDs may have exited already and
+    # this causes a NoSuchProcess exception.
     cmdlines = []
     for proc in procs:
         try:
-            # In psutil 1.2.1, cmdline is an attribute, but in 5.x, it's now
-            # a callable method.
+            # In psutil 1.2.1, cmdline is an attribute, but in
+            # 5.x, it's now a callable method.
             cmdline_check = getattr(proc, "cmdline", None)
             if callable(cmdline_check):
-                cmdlines.append(map(os.path.basename, proc.cmdline()))
+                cmdlines.append(
+                    map(os.path.basename, proc.cmdline())
+                )
             else:
-                cmdlines.append(map(os.path.basename, proc.cmdline))
+                cmdlines.append(map(os.path.basename,
+                                    proc.cmdline))
         except Exception as e:
-            status_err('Error while retrieving process %s' %
-                       proc.cmdline,
+            status_err('Error while retrieving process %s, exception: %s'
+                       % (proc.cmdline, str(e)),
                        m_name='maas_neutron')
             pass
 
-    # Loop through the process names provided on the command line to see
-    # if ovsdb-server,  ovs-vswitchd, and neutron-openvswitch exist on
-    # the system or in a container.
+    # Loop through the process names provided on the command line to
+    # see if ovsdb-server,  ovs-vswitchd, and neutron-openvswitch
+    # exist on the system or in a container.
     # suppress some character which throw MaaS off
-    # ovsdb-server and ovs-vswitchd are not directly in the command line
-    # parsing so we use condition
+    # ovsdb-server and ovs-vswitchd are not directly in the command
+    # line parsing so we use condition
     # `process_name in x or process_name in x[0]`
-    process_names = ['ovsdb-server', 'ovs-vswitchd',
-                     'neutron-openvswitch-agent']
     pattern = re.compile('[^-\w]+')
     for process_name in process_names:
         matches = [x for x in cmdlines
-                   if process_name in x or process_name in x[0]
+                   if process_name in x or (
+                       len(x) > 0 and process_name in x[0]
+                   )
                    ]
 
-        metric_bool('%s_process_status' % pattern.sub('', process_name),
+        metric_bool('%s_process_status_on_host_%s' % (
+                    pattern.sub('', process_name),
+                    container_or_host_name
+                    ),
                     len(matches) > 0)
+
+
+def check(args):
+
+    if on_lxc_container:
+        containers = lxc.list_containers()
+        neutron_agent_containers = []
+        for container in containers:
+            if 'neutron_agents' in container:
+                metric_bool('agents_found_on_host_%s' %
+                            container,
+                            True, m_name='maas_neutron')
+                neutron_agent_containers.append(container)
+
+        if len(neutron_agent_containers) == 0:
+            metric_bool('agents_found', False, m_name='maas_neutron')
+            status_err('no running neutron agents containers found',
+                       m_name='maas_neutron')
+            return
+
+        for neutron_agent_container in neutron_agent_containers:
+            # Get the neutron_agent_container's init PID.
+            try:
+                c = lxc.Container(neutron_agent_container)
+                # If the container wasn't found, exit now.
+                if c.init_pid == -1:
+                    metric_bool('container_success_on_host_%s' %
+                                neutron_agent_container,
+                                False,
+                                m_name='maas_neutron_agent_container')
+                    status_err(
+                        'Could not find PID for container {}'.format(
+                            neutron_agent_container
+                        ),
+                        m_name='maas_neutron_agent_container'
+                    )
+            except (Exception, SystemError) as e:
+                metric_bool('container_success', False,
+                            m_name='maas_neutron_agent_container')
+                status_err(
+                    'Container lookup failed on "{}". ERROR: "{}"'
+                    .format(
+                        neutron_agent_container,
+                        e
+                    ),
+                    m_name='maas_neutron_agent_container'
+                )
+            else:
+                metric_bool('container_success_on_host_%s' %
+                            neutron_agent_container, True,
+                            m_name='maas_neutron_agent_container')
+
+            # c is the lxc container instance of this
+            # neutron_agent_container
+            check_process_statuses(neutron_agent_container, c)
+    else:
+        ovs_agent_host = socket.gethostname()
+        check_process_statuses(ovs_agent_host)
 
 
 def main(args):
