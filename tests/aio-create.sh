2757ce8ee2653bb0f89775e494ee2a3fc7a15f23
Andy McCrae <andy.mccrae@gmail.com>
2017-12-13 17:14:22 +1000

    [DNM] Testing with 2 ceph osds
    MaaS tests fail with 2 OSDs, but the rpc-ceph repo is working fine for
    that when running MaaS.
    
    This PR is to test that and see what failures occur.

--- tests/aio-create.sh
+++ tests/aio-create.sh
@@ -128,7 +128,7 @@ if [ "${IRR_CONTEXT}" == "ceph" ]; then
     rm -rf /opt/rpc-ceph
   fi
 
-  git clone https://github.com/rcbops/rpc-ceph /opt/rpc-ceph
+  git clone https://github.com/andymcc/rpc-ceph /opt/rpc-ceph -b 2osd
   export RPC_MAAS_DIR="$(pwd)"
   pushd /opt/rpc-ceph
     bash /opt/rpc-ceph/run_tests.sh
