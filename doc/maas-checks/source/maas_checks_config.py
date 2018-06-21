15a859e03fc65be21500ab89c7674552f7fc7b60
Kevin Carter <kevin.carter@rackspace.com>
2018-06-20 22:33:56 -0500

    Remove the elasticsearch maas check
    The elasticsearch maas check is not doing a whole lot and is more noise
    than value. This is on top of the fact that the elasticsearch checks are
    not made for a deployment where elasticsearch/logstash are deployed in a
    high performance cluster.
    
    This change follows on with the removal of the filebeat check.
    
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

--- doc/maas-checks/source/maas_checks_config.py
+++ doc/maas-checks/source/maas_checks_config.py
@@ -163,8 +163,7 @@ def _get_globals(config_variables):
     # configured list.
     # NOTE: Don't copy config_variables into new_globals wholesale as we
     # want things such as alarm criteria to remain as actual template values.
-    for name in ("elasticsearch_process_names",
-                 "ceph_radosgw_protocol",
+    for name in ("ceph_radosgw_protocol",
                  "rsyslogd_process_names",
                  "maas_swift_account_process_names",
                  "maas_swift_container_process_names",
