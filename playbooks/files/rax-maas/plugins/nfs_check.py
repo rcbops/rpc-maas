f375a628599f03df081f029241ae2c691bfb8ad7
cloudnull <kevin.carter@rackspace.com>
2018-10-23 23:07:11 -0500

    [TURTLES-1547] Add check for NFS
    This is a new check added to the maas system allowing us to collect
    metrics on the state of NFS from within a cloud deployment. The new
    check will search for anything running NFS on a given host and report
    the number of exports and the state of the connection. If any
    connection becomes unavailable an alarm will be triggered noting that
    the state of NFS has become "degarded".
    
    Metrics:
      - nfs_all_online: bool
      - nfs_{}_exports: int
      - nfs_{}_online: bool
    
    With these metrics we'll be able to determine export changes as seen
    from the host machine, the state of a given server connection, and
    the state of all server connections.
    
    Signed-off-by: cloudnull <kevin.carter@rackspace.com>
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

---
+++ playbooks/files/rax-maas/plugins/nfs_check.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+# Copyright 2018, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import shlex
+import subprocess
+
+from maas_common import metric
+from maas_common import metric_bool
+from maas_common import print_output
+from maas_common import status_err
+from maas_common import status_ok
+
+
+def nfs_export_check():
+    nfs_metrics = dict()
+
+    output = subprocess.check_output(
+        shlex.split(
+            "awk -F':' '/nfs/ {print $1}' /proc/mounts"
+        )
+    )
+    nfs_mounts = output.splitlines()
+    for mount in nfs_mounts:
+        mount_metrics = nfs_metrics[mount] = {'exports': 0, 'online': False}
+        try:
+            exports = subprocess.check_output(
+                shlex.split(
+                    "showmount --no-headers --exports {}".format(mount)
+                )
+            )
+        except subprocess.CalledProcessError:
+            mount_metrics['exports'] = 0
+        else:
+            mount_metrics['exports'] += len(exports.splitlines())
+
+        if mount_metrics['exports'] > 0:
+            mount_metrics['online'] = True
+
+    return nfs_metrics
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Disk utilisation checks')
+    parser.add_argument('--telegraf-output',
+                        action='store_true',
+                        default=False,
+                        help='Set the output format to telegraf')
+    args = parser.parse_args()
+    with print_output(print_telegraf=args.telegraf_output):
+        nfs_system_check = nfs_export_check()
+
+        # If there are no returns for the nfs check or the status is ALL
+        # online the check is marked as "OK".
+        status = all([i['online'] for i in nfs_system_check.values()])
+        if status:
+            status_ok(m_name='nfs_check')
+        else:
+            status_err('The state of NFS connections is degraded',
+                       m_name='nfs_check')
+
+        # Generate a general purpose metric fot the operational state of NFS.
+        metric_bool('nfs_all_online', status, m_name='nfs_check')
+
+        for key, value in nfs_system_check.items():
+            # Generate a metric for the number of exports seen from a server.
+            metric('nfs_{}_exports'.format(key),
+                   'uint32',
+                   value['exports'],
+                   'exports',
+                   m_name='nfs_check')
+
+            # Generate a metric for the given online or offline state.
+            metric_bool('nfs_{}_online'.format(key),
+                        value['online'],
+                        m_name='nfs_check')
