25d5eafe77de64ecc298699d0d47b97ff2cfa86f
Your Name <you@example.com>
2019-01-18 01:01:15 +0000

    Update for more rocky and embedded ansible changes

--- tests/maasutils.py
+++ tests/maasutils.py
@@ -1,31 +1,51 @@
 #!/usr/bin/env python
+import argparse
+import json
 import os
+import sys
 
-import click
-import pyrax
 import requests
 
 
-@click.option("--username", required=True)
-@click.option("--api-key", required=True)
-@click.group()
-def cli(api_key, username):
-    pyrax.set_setting("identity_type", "rackspace")
-    pyrax.set_credentials(
-        username,
-        api_key
-    )
+# Query public identity with a username and apikey
+def auth(username, apikey):
+    url = 'https://identity.api.rackspacecloud.com/v2.0/tokens'
+    headers = {"Content-type": "application/json"}
+    data = {
+        "auth": {
+            "RAX-KSKEY:apiKeyCredentials": {
+                "username": username,
+                "apiKey": apikey
+            }
+        }
+    }
+
+    try:
+        r = requests.post(url, headers=headers, json=data)
+        r.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print e
+        sys.exit(1)
+    except requests.exceptions.HTTPError as httpe:
+        print httpe
+        sys.exit(1)
+    return r.json()
 
 
-@click.command(name='get_token_url')
-def get_token_url():
-    token = pyrax.identity.token
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Obtain maas_api_url and'
+                                     'maas_auth_token from a username and'
+                                     'apikey')
+    parser.add_argument('--username', type=str, help='RAX cloud username')
+    parser.add_argument('--apikey', type=str, help='RAX cloud apikey')
+    args = parser.parse_args()
 
-    monitoring_service = next(
-        b for b in pyrax.identity.service_catalog
-        if b["type"] == "rax:monitor"
-    )
-    url = monitoring_service["endpoints"][0]["publicURL"]
+    resp = auth(args.username, args.apikey)
+    token = resp['access']['token']['id']
+    for service in resp['access']['serviceCatalog']:
+        if service['name'] == 'cloudMonitoring':
+            url = service['endpoints'][0]['publicURL']
+
     cred_file = os.path.expanduser('~/maas-vars.rc')
     with open(cred_file, 'w') as f:
         f.write(
@@ -35,51 +55,4 @@ def get_token_url():
                 url=url
             )
         )
-
-    click.echo(
-        'Credentials file written to "{cred_file}"'.format(
-            cred_file=cred_file
-        )
-    )
-    return token, url
-
-
-@click.command(name='set_webhook_token')
-@click.option("--token", 'webhook_token', required=True)
-@click.pass_context
-def set_webhook_token(ctx, webhook_token):
-    """Sets the token that is included in MaaS webhook notifications
-
-    This is one method of verifying that receieved requests are
-    from MaaS. This is per account.
-    """
-    auth_token, url = ctx.invoke(get_token_url)
-    try:
-        response = requests.put(
-            "{url}/account".format(url=url),
-            headers={'X-Auth-Token': auth_token},
-            json={'webhook_token': webhook_token})
-        response.raise_for_status()
-        click.echo("Webhook token set to {}".format(webhook_token))
-    except requests.exceptions.HTTPError as e:
-        click.echo(response.text)
-        raise e
-
-
-@click.command(name='get_entity_id')
-@click.option("--label", help="label of entity to get ID for", required=True)
-def get_entity_id(label):
-    mon = pyrax.cloud_monitoring
-    entities = mon.list_entities()
-    for e in entities:
-        if label == e.label:
-            click.echo(e.id)
-
-
-cli.add_command(get_token_url)
-cli.add_command(set_webhook_token)
-cli.add_command(get_entity_id)
-
-
-if __name__ == "__main__":
-    cli()
+    print('Credentials file written to "%s"') % cred_file
