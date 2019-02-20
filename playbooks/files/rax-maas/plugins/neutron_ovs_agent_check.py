8b78a01a834a045615a670d28991498de82b2ee6
tonytan4ever <tony.tan@rackspace.com>
2019-02-19 19:01:26 -0600

    NATT-72: Improve the stability of neutron ovs agent check
    Signed-off-by: tonytan4ever <tony.tan@rackspace.com>

--- playbooks/files/rax-maas/plugins/neutron_ovs_agent_check.py
+++ playbooks/files/rax-maas/plugins/neutron_ovs_agent_check.py
@@ -24,15 +24,16 @@ except ImportError:
     import socket
     on_lxc_container = False
 
+from maas_common import get_neutron_client
+from maas_common import metric
 from maas_common import metric_bool
 from maas_common import print_output
-from maas_common import status_err, status_err_no_exit
+from maas_common import status_err, status_err_no_exit, status_ok
 from process_check_host import get_processes
 
 
 def check_process_statuses(container_or_host_name, container=None):
-    process_names = ['ovsdb-server', 'ovs-vswitchd',
-                     'neutron-openvswitch-agent']
+    process_names = ['ovsdb-server', 'ovs-vswitchd']
 
     if container is None:
         pid = None
@@ -86,17 +87,68 @@ def check_process_statuses(container_or_host_name, container=None):
 
 def check(args):
 
+    NETWORK_ENDPOINT = '{protocol}://{hostname}:9696'.format(
+        protocol=args.protocol, hostname=args.hostname)
+    try:
+        neutron = get_neutron_client(endpoint_url=NETWORK_ENDPOINT)
+
+    # not gathering api status metric here so catch any exception
+    except Exception as e:
+        metric_bool('client_success', False, m_name='maas_neutron')
+        metric('%s_status' % "neutron-openvswitch-agent",
+               'string',
+               '%s cannot reach API' % "neutron-openvswitch-agent",
+               m_name='maas_neutron')
+        status_err_no_exit(str(e), m_name='maas_neutron')
+        return
+    else:
+        metric_bool('client_success', True, m_name='maas_neutron')
+
+    # gather neutron service states
+    if args.host:
+        agents = neutron.list_agents(host=args.host)['agents']
+    elif args.fqdn:
+        agents = neutron.list_agents(host=args.fqdn)['agents']
+    else:
+        agents = neutron.list_agents()['agents']
+
+    try:
+        ovs_agent = next(
+            a for a in agents if 'openvswitch' in a['binary']
+        )
+    except StopIteration:
+        metric_bool('agents_found', False, m_name='maas_neutron')
+        status_err("No host(s) found in the agents list",
+                   m_name='maas_neutron')
+    else:
+        metric_bool('agents_found', True, m_name='maas_neutron')
+
+    # return all the things
+    status_ok(m_name='maas_neutron')
+
+    agent_is_up = "Yes"
+    if ovs_agent['admin_state_up'] and not ovs_agent['alive']:
+            agent_is_up = "No"
+
+    if args.host:
+        name = '%s_status' % ovs_agent['binary']
+    elif args.fqdn:
+        name = '%s_status' % ovs_agent['binary']
+    else:
+        name = '%s_%s_on_host_%s' % (ovs_agent['binary'],
+                                     ovs_agent['id'],
+                                     ovs_agent['host'])
+
+    metric(name, 'string', agent_is_up, m_name='maas_neutron')
+
     if on_lxc_container:
         containers = lxc.list_containers()
         neutron_agent_containers = []
         for container in containers:
             if 'neutron_agents' in container:
-                metric_bool('agents_found',
-                            True, m_name='maas_neutron')
                 neutron_agent_containers.append(container)
 
         if len(neutron_agent_containers) == 0:
-            metric_bool('agents_found', False, m_name='maas_neutron')
             status_err('no running neutron agents containers found',
                        m_name='maas_neutron')
             return
@@ -146,6 +198,21 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Check neutron OVS'
                                                  'agents running')
+    parser.add_argument('hostname',
+                        type=str,
+                        help='Neutron API hostname or IP address')
+    parser.add_argument('--host',
+                        type=str,
+                        help='Only return metrics for specified host',
+                        default=None)
+    parser.add_argument('--fqdn',
+                        type=str,
+                        help='Only return metrics for specified fqdn',
+                        default=None)
+    parser.add_argument('--protocol',
+                        type=str,
+                        default='http',
+                        help='Protocol for client requests')
     parser.add_argument('--telegraf-output',
                         action='store_true',
                         default=False,
