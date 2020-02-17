4925bca70c084b3bda04ecf11c0400cc818db762
Nathan Pawelek <nathan.pawelek@rackspace.com>
2020-02-17 10:59:17 -0600

    Update basic tests to allow deployment of OSA Train
    This change includes basic installation from tests scripts on a OSA
    Train deployment.
    
    Signed-off-by: Nathan Pawelek <nathan.pawelek@rackspace.com>

--- tests/aio-create.sh
+++ tests/aio-create.sh
@@ -374,6 +374,10 @@ else
       elif [[ "${RE_JOB_SCENARIO}" == "stein" ]]; then
         git checkout "stable/stein"  # Branch checkout of Stein (Current Stable)
         export ANSIBLE_INVENTORY="/opt/openstack-ansible/inventory"
+
+      elif [[ "${RE_JOB_SCENARIO}" == "train" ]]; then
+        git checkout "stable/train"  # Branch checkout of Train (Current Stable)
+        export ANSIBLE_INVENTORY="/opt/openstack-ansible/inventory"
       fi
 
       # Install ovs agent if applicable
@@ -391,7 +395,7 @@ else
       # NOTE(npawelek): Rocky requires a different version to be set, all other
       # releases work with the existing pin
       case ${RE_JOB_SCENARIO} in
-        rocky|stein)
+        rocky|stein|train)
           true
           ;;
         *)
